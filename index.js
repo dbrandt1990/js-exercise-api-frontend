4d0bfa40aa6073b4ea63bae3bec38681f90c7079
dbrandt1990 <dvbrandt90@gmail.com>
2021-06-10 13:27:26 -0600

    changed category to category_id

--- index.js
+++ index.js
@@ -29,27 +29,27 @@ function createExerciseHandler(e) {
     e.preventDefault()
     let name = document.querySelector("#name").value
     let description = document.querySelector("#description").value
-    let category = parseInt(document.querySelector("#category").value)
+    let category_id = parseInt(document.querySelector("#category").value)
 
-    postFetch(name, description, category)
+    postFetch(name, description, category_id)
 
 }
 
-function postFetch(name, description, category) {
+function postFetch(name, description, category_id) {
     const data = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             name: name,
             description: description,
-            category: category
+            category_id: category_id
         })
     }
 
     fetch(EXERCISES_URL, data)
         .then(response => response.json())
         .then(exercise => {
-
+            console.log(exercise)
         })
 }
 
